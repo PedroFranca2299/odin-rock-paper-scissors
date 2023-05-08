bee8c7e321e150df9106f2fea26f17b4eaf5ab57
Pedro Henrique <pedronutti@gmail.com>
2023-05-08 13:10:15 -0300

    Add function for player choice and function for one round

--- script.js
+++ script.js
@@ -10,4 +10,35 @@ function getComputerChoice (randomInteger) {
         return "Scissors";
     }
 }
-console.log(getComputerChoice(randomInteger));
+
+const computerChoice = getComputerChoice(randomInteger); 
+
+console.log(computerChoice);
+
+const playerSelection = prompt("Choose rock, paper or scissors!");
+
+function selectionCase (playerSelection) {
+    return playerSelection.toLowerCase();
+}
+
+const playerChoice = selectionCase(playerSelection);
+
+console.log(selectionCase(playerSelection));
+
+function round (playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "Scissors") {
+        return "You win! Rock beats scissors!";
+    } else if (playerChoice === "scissors" && computerChoice === "Rock") {
+        return "You lose! Rock beats scissors!";
+    } else if (playerChoice === "scissors" && computerChoice === "Paper") {
+        return "You win! Scissors beats paper!";
+    } else if (playerChoice === "paper" && computerChoice === "Scissors") {
+        return "You lose! Scissors beats paper!";
+    } else if (playerChoice === "paper" && computerChoice === "Rock") {
+        return "You win! Paper beats rock!";
+    } else if (playerChoice === "rock" && computerChoice === "Paper") {
+        return "You lose! Paper beats rock!";
+    }
+}
+
+console.log(round(playerChoice, computerChoice));
